f15e514439a078120963d75b87d78ddc710e5424
Isaac Nyamari <jablessions76@.com>
2025-06-22 01:52:30 +0300

    edited by isaac

--- header.php
+++ header.php
@@ -31,26 +31,4 @@
 </head>
 
 <body>
-    <!-- Navbar Start -->
-    <div class="container-fluid bg-light position-relative shadow">
-        <nav class="navbar navbar-expand-lg bg-light navbar-light py-3 py-lg-0 px-0 px-lg-5">
-            <a href="" class="navbar-brand font-weight-bold text-danger" style="font-size: 50px;">
-                <img src="image.png" alt="" class="logo">
-                <span class="text-primary">Y K A</span>
-            </a>
-            <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
-                <div class="navbar-nav font-weight-bold mx-auto py-0">
-                    <a href="index.html" class="nav-item nav-link active">Home</a>
-                    <a href="#" class="nav-item nav-link">Dashboard</a>
-                    <a href="#" class="nav-item nav-link">Classes</a>
-                    <a href="#" class="nav-item nav-link">Contact</a>
-                </div>
-                <a href="" class="btn btn-success px-4 mr-1"><i class="fa fa-user-plus" aria-hidden="true"></i> Join Us</a>
-                <a href="" class="btn btn-primary px-4"><i class="fa fa-sign-in" aria-hidden="true"></i> Login</a>
-            </div>
-        </nav>
-    </div>
-    <!-- Navbar End -->
+<?php include 'navbar.php'; ?>
---
+++ index.php
@@ -0,0 +1,289 @@
+<?php require "header.php" ?>
+<!-- banner Start -->
+<div class="container-fluid bg-primary px-0 px-md-5 mb-5">
+    <div class="row align-items-center px-3">
+        <div class="col-lg-6 text-center text-lg-left">
+            <h4 class="text-white mb-4 mt-5 mt-lg-0">Affordable Online Courses and Learning Opportunities​</h4>
+            <h1 class="display-3 font-weight-bold text-white">Education for everyone</h1>
+            <p class="text-white mb-4">Finding your own space and utilize better learning options can result in
+                faster than the traditional ways. Enjoy the beauty of eLearning!</p>
+            <a href="" class="btn btn-danger mt-1 py-3 px-5"> <i class="fa fa-user-plus" aria-hidden="true"></i> Join Us</a>
+            <a href="" class="btn btn-secondary mt-1 py-3 px-5"><i class="fa fa-sign-in" aria-hidden="true"></i>
+                Login</a>
+        </div>
+        <div class="col-lg-6 text-center text-lg-right">
+            <img class="img-fluid mt-5" src="img/header.png" alt="">
+        </div>
+    </div>
+</div>
+<!-- banner End -->
+
+
+<!-- Facilities Start -->
+<div class="container-fluid pt-5">
+    <div class="container pb-3">
+        <div class="row">
+            <div class="col-lg-4 col-md-6 pb-1">
+                <div class="d-flex bg-light shadow-sm border-top rounded mb-4" style="padding: 30px;">
+                    <i class="flaticon-050-fence h1 font-weight-normal text-primary mb-3"></i>
+                    <div class="pl-4">
+                        <h4>Learn the Latest Top Skills</h4>
+                        <p class="m-0">Learning top skills can bring an extra-ordinary outcome in a career.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 pb-1">
+                <div class="d-flex bg-light shadow-sm border-top rounded mb-4" style="padding: 30px;">
+                    <i class="flaticon-022-drum h1 font-weight-normal text-primary mb-3"></i>
+                    <div class="pl-4">
+                        <h4>Learn in Your Own Time</h4>
+                        <p class="m-0">Everyone prefers to enjoy learning at their own pace & that gives a great
+                            result.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 pb-1">
+                <div class="d-flex bg-light shadow-sm border-top rounded mb-4" style="padding: 30px;">
+                    <i class="flaticon-030-crayons h1 font-weight-normal text-primary mb-3"></i>
+                    <div class="pl-4">
+                        <h4>Arts and Crafts</h4>
+                        <p class="m-0">Our Arts & Crafts Program gives learners the freedom to express themselves.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 pb-1">
+                <div class="d-flex bg-light shadow-sm border-top rounded mb-4" style="padding: 30px;">
+                    <i class="flaticon-017-toy-car h1 font-weight-normal text-primary mb-3"></i>
+                    <div class="pl-4">
+                        <h4>Learn From Experts</h4>
+                        <p class="m-0">Experienced teachers can assist in learning faster with their best
+                            approaches!</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 pb-1">
+                <div class="d-flex bg-light shadow-sm border-top rounded mb-4" style="padding: 30px;">
+                    <i class="flaticon-025-sandwich h1 font-weight-normal text-primary mb-3"></i>
+                    <div class="pl-4">
+                        <h4>Learn From Anywhere</h4>
+                        <p class="m-0">We are delighted to give you options to enjoy learning from anywhere in the
+                            world.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 pb-1">
+                <div class="d-flex bg-light shadow-sm border-top rounded mb-4" style="padding: 30px;">
+                    <i class="flaticon-047-backpack h1 font-weight-normal text-primary mb-3"></i>
+                    <div class="pl-4">
+                        <h4>Educational Tour</h4>
+                        <p class="m-0">At Yeah Kenyan Academy, we believe that education goes far beyond textbooks.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Facilities Start -->
+
+
+<!-- About Start -->
+<div class="container-fluid py-5">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-5">
+                <img class="img-fluid rounded mb-5 mb-lg-0" src="img/about-1.jpg" alt="">
+            </div>
+            <div class="col-lg-7">
+                <p class="section-title pr-5"><span class="pr-2">Learn About Us</span></p>
+                <h1 class="mb-4">Best School For Your Kids</h1>
+                <p>At Yeah Kenyan Academy, we believe every child deserves the best foundation for a successful
+                    future. Our commitment to academic excellence, character development, and holistic growth makes
+                    us the preferred choice for families seeking quality education in Kenya.</p>
+                <div class="row pt-2 pb-4">
+                    <div class="col-6 col-md-4">
+                        <img class="img-fluid rounded" src="img/about-2.jpg" alt="">
+                    </div>
+                    <div class="col-6 col-md-8">
+                        <ul class="list-inline m-0">
+                            <li class="py-2 border-top border-bottom">
+                                <i class="fa fa-check text-primary mr-3"></i>
+                                Nurturing creativity and critical thinking in every child.
+                            </li>
+                            <li class="py-2 border-bottom">
+                                <i class="fa fa-check text-primary mr-3"></i>
+                                Providing a safe and supportive learning environment.
+                            </li>
+                            <li class="py-2 border-bottom">
+                                <i class="fa fa-check text-primary mr-3"></i>
+                                Encouraging curiosity and a lifelong love for learning.
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <a href="" class="btn btn-primary mt-2 py-2 px-4">Learn More</a>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- About End -->
+
+
+<!-- Class Start -->
+<div class="container-fluid pt-5">
+    <div class="container">
+        <div class="text-center pb-2">
+            <p class="section-title px-5"><span class="px-2">CBC Compliant</span></p>
+            <h1 class="mb-4">CBC Classes Available</h1>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 mb-5">
+                <div class="card border-0 bg-light shadow-sm pb-2">
+                    <img class="card-img-top mb-2" src="img/class-1.jpg" alt="">
+                    <div class="card-body text-center">
+                        <h4 class="card-title">Pre - Primary</h4>
+                        <p class="card-text">
+                            Our pre-primary program is designed to foster creativity, curiosity, and a love for
+                            learning from an early age.
+                        </p>
+                    </div>
+                    <div class="card-footer bg-transparent py-4 px-5">
+                        <div class="row border-bottom">
+                            <div class="col-6 py-1 text-right border-right"><strong>Age of Kids</strong></div>
+                            <div class="col-6 py-1">1 - 2 Years</div>
+                        </div>
+                        <div class="row border-bottom">
+                            <div class="col-6 py-1 text-right border-right"><strong>Total Seats</strong></div>
+                            <div class="col-6 py-1">40 Seats</div>
+                        </div>
+                        <div class="row border-bottom">
+                            <div class="col-6 py-1 text-right border-right"><strong>Class Time</strong></div>
+                            <div class="col-6 py-1">08:00 - 10:00</div>
+                        </div>
+                        <div class="row">
+                            <div class="col-6 py-1 text-right border-right"><strong>Tution Fee</strong></div>
+                            <div class="col-6 py-1">$290 / Month</div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <div class="col-lg-4 mb-5">
+                <div class="card border-0 bg-light shadow-sm pb-2">
+                    <img class="card-img-top mb-2" src="img/class-1.jpg" alt="">
+                    <div class="card-body text-center">
+                        <h4 class="card-title">Grade 1 - Grade 9</h4>
+                        <p class="card-text">Entails lower and upper primary education, focusing on core subjects
+                            and holistic development.</p>
+                    </div>
+                    <div class="card-footer bg-transparent py-4 px-5">
+                        <div class="row border-bottom">
+                            <div class="col-6 py-1 text-right border-right"><strong>Age of Kids</strong></div>
+                            <div class="col-6 py-1">3 - 6 Years</div>
+                        </div>
+                        <div class="row border-bottom">
+                            <div class="col-6 py-1 text-right border-right"><strong>Total Seats</strong></div>
+                            <div class="col-6 py-1">40 Seats</div>
+                        </div>
+                        <div class="row border-bottom">
+                            <div class="col-6 py-1 text-right border-right"><strong>Class Time</strong></div>
+                            <div class="col-6 py-1">08:00 - 10:00</div>
+                        </div>
+                        <div class="row">
+                            <div class="col-6 py-1 text-right border-right"><strong>Tution Fee</strong></div>
+                            <div class="col-6 py-1">$290 / Month</div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <div class="col-lg-4 mb-5">
+                <div class="card border-0 bg-light shadow-sm pb-2">
+                    <img class="card-img-top mb-2" src="img/class-2.jpg" alt="">
+                    <div class="card-body text-center">
+                        <h4 class="card-title">Grade 10,11,12 (Secondary)</h4>
+                        <p class="card-text">Justo ea diam stet diam ipsum no sit, ipsum vero et et diam ipsum duo
+                            et no et, ipsum ipsum erat duo amet clita duo</p>
+                    </div>
+                    <div class="card-footer bg-transparent py-4 px-5">
+                        <div class="row border-bottom">
+                            <div class="col-6 py-1 text-right border-right"><strong>Age of Kids</strong></div>
+                            <div class="col-6 py-1">3 - 6 Years</div>
+                        </div>
+                        <div class="row border-bottom">
+                            <div class="col-6 py-1 text-right border-right"><strong>Total Seats</strong></div>
+                            <div class="col-6 py-1">40 Seats</div>
+                        </div>
+                        <div class="row border-bottom">
+                            <div class="col-6 py-1 text-right border-right"><strong>Class Time</strong></div>
+                            <div class="col-6 py-1">08:00 - 10:00</div>
+                        </div>
+                        <div class="row">
+                            <div class="col-6 py-1 text-right border-right"><strong>Tution Fee</strong></div>
+                            <div class="col-6 py-1">$290 / Month</div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+<!-- Class End -->
+
+
+<!-- Registration Start -->
+<div class="container-fluid py-5">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-7 mb-5 mb-lg-0">
+                <p class="section-title pr-5"><span class="pr-2">Student Enrollment</span></p>
+                <h1 class="mb-4">Enroll Your Child Today</h1>
+                <p>Join Yeah Kenyan Academy and give your child the best start to their educational journey. Our enrollment process is simple and designed to help you secure a place for your child in our vibrant learning community.</p>
+                <ul class="list-inline m-0">
+                    <li class="py-2"><i class="fa fa-check text-success mr-3"></i>Holistic and quality education</li>
+                    <li class="py-2"><i class="fa fa-check text-success mr-3"></i>Experienced and caring teachers</li>
+                    <li class="py-2"><i class="fa fa-check text-success mr-3"></i>Modern facilities and resources</li>
+                </ul>
+                <a href="" class="btn btn-primary mt-4 py-2 px-4">Enroll Now</a>
+            </div>
+            <div class="col-lg-5">
+                <div class="card border-0">
+                    <div class="card-header bg-secondary text-center p-4">
+                        <h1 class="text-white m-0">Student Enrollment</h1>
+                    </div>
+                    <div class="card-body rounded-bottom bg-primary p-5">
+                        <form>
+                            <div class="form-group">
+                                <input type="text" class="form-control border-0 p-4" placeholder="Student Name"
+                                    required="required" />
+                            </div>
+                            <div class="form-group">
+                                <input type="email" class="form-control border-0 p-4" placeholder="Parent Email"
+                                    required="required" />
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control border-0 p-4" placeholder="Parent Phone"
+                                    required="required" />
+                            </div>
+                            <div class="form-group">
+                                <select class="custom-select border-0 px-4" style="height: 47px;">
+                                    <option selected>Select Class</option>
+                                    <option value="pre-primary">Pre-Primary</option>
+                                    <option value="grade-1-9">Grade 1 - Grade 9</option>
+                                    <option value="secondary">Grade 10, 11, 12 (Secondary)</option>
+                                </select>
+                            </div>
+                            <div>
+                                <button class="btn btn-secondary btn-block border-0 py-3" type="submit">Enroll Now</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Registration End -->
+<?php require "footer.php" ?>
