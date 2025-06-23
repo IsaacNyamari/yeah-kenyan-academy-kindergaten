00d4e3b2fe246e12102d6fbb7e05a32fb4a26ef0
Isaac Nyamari <jablessions76@.com>
2025-06-23 22:14:08 +0300

    edited by isaac

--- auth/footer.php
+++ auth/footer.php
@@ -5,6 +5,9 @@
         <p><i class="fa fa-play"></i> Guide</p>
     </div>
 </div>
+<!-- bootstrap js and jquery here -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 <script src="./tour.js"> </script>
 </body>
 
--- auth/navbar.php
+++ auth/navbar.php
@@ -6,21 +6,34 @@
         <li class="nav-item">
             <a class="nav-link" title="dashboard" href="#"><i class="fa fa-dashboard" aria-hidden="true"></i> <span class="menu-name">Dashboard</span></a>
         </li>
+        <?php if ($role === "student") { ?>
         <li class="nav-item">
             <a class="nav-link" title="progress" href="#"><i class="fa fa-graduation-cap" aria-hidden="true"></i> <span class="menu-name">Progress</span></a>
         </li>
-
+        <?php } ?>
+        <?php if ($role === "teacher") { ?>
+            <li class="nav-item dropend bg-dark">
+                <a class="nav-link bg-dark dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <i class="fas fa-user-graduate"></i> Students
+                </a>
+                <ul class="dropdown-menu bg-dark" aria-labelledby="navbarDropdown">
+                    <li><a class="dropdown-item" href="#">Add Progress</a></li>
+                    <li><a class="dropdown-item" href="#"><i class="fas fa-eye    "></i> View Stdents</a></li>
+                    <li><a class="dropdown-item" href="#"><i class="fas fa-user-edit    "></i> Edit student</a></li>
+                </ul>
+            </li>
+        <?php } ?>
         <li class="nav-item">
             <a class="nav-link" title="time-table" href="#"><i class="fa fa-calendar-check-o" aria-hidden="true"></i> <span class="menu-name">Time-table</span></a>
         </li>
         <li class="nav-item">
             <a class="nav-link" title="me" href="#"><i class="fa fa-user-circle-o" aria-hidden="true"></i> <span class="menu-name">Me</span></a>
         </li>
-       <?php if($role === "admin"){ ?>
-        <li class="nav-item">
-            <a class="nav-link" title="settings" href="#"><i class="fa fa-tools" aria-hidden="true"></i> <span class="menu-name">Settings</span></a>
-        </li>
-       <?php }?>
+        <?php if ($role === "admin") { ?>
+            <li class="nav-item">
+                <a class="nav-link" title="settings" href="#"><i class="fa fa-tools" aria-hidden="true"></i> <span class="menu-name">Settings</span></a>
+            </li>
+        <?php } ?>
         <li class="nav-item">
             <a class="nav-link" title="exit" href="#"><i class="fa fa-sign-out" aria-hidden="true"></i> <span class="menu-name">Exit</span></a>
         </li>
